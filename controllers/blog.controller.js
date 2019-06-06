e7cc282c62af0e36ab89d9b075a7fd7bf99c2eed
Verholantsev Artem <averholantsev@ics.perm.ru>
2019-06-06 15:49:56 +0500

    img upload

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog.model');
+const fs = require('fs');
 
 exports.blog_list = (req, res) => {
     var pageNo = parseInt(req.query.page);
@@ -49,20 +50,25 @@ exports.blog_record = (req, res) => {
 };
 
 exports.blog_create = (req, res, next) => {
-  let blog = new Blog({
-      autor: req.body.autor,
-      date: req.body.date,
-      main_img_path: req.body.main_img_path,
-      tag_name: req.body.tag_name,
-      title: req.body.title,
-      description: req.body.description
-  });
-  console.log(req.files.fileup.name)
+    let sampleFile = req.files.fileup;
 
-/*   blog.save((err) => {
-      if (err) {
-          return next (err);
-      };
-      res.send('Blog created successfully');
-  }); */
+    sampleFile.mv(`./public/src/${sampleFile.name}`, (err) => {
+        if (err)
+            return res.status(500).send(err);
+    })
+    let blog = new Blog({
+        autor: req.body.autor,
+        date: req.body.date,
+        main_img_path: `/src/${sampleFile.name}`,
+        tag_name: req.body.tag_name,
+        title: req.body.title,
+        description: req.body.description
+    });
+  
+    blog.save((err) => {
+        if (err) {
+            return next (err);
+        };
+        res.send('Blog created successfully');
+    });
 };
